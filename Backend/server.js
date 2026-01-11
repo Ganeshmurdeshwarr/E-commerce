49e744d0151421bc1959c45577f81c2f9974cf83
Ganesh Devadiga <ganeshmurdeshwar@gmail.com>
2026-01-11 12:39:43 +0530

    cors postion changed

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,13 @@
-const express = require("express")
-const cors = require("cors")
-const dotenv = require("dotenv")
-const connectDB = require("./config/db")
-const userRoutes = require("./routes/userRoutes")
-const productRouter = require("./routes/productRoutes")
-const cartRouter = require("./routes/cartRouter")
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
+const productRouter = require("./routes/productRoutes");
+const cartRouter = require("./routes/cartRouter");
 const checkoutRouter = require("./routes/checkoutRouter");
 const orderRouter = require("./routes/orderRouter");
 const uploadRouter = require("./routes/uploadRoutes");
@@ -14,17 +17,22 @@ const productAdminRoutes = require("./routes/productAdminRoutes");
 const adminOrderRoutes = require("./routes/adminOrderRoutes");
 
 
-const app = express()
-app.use(express.json())
-app.use(cors())
 
-dotenv.config()
-const PORT = process.env.PORT || 3000
+const app = express();
+app.use(express.json());
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://e-commerce-p1y6.vercel.app"],
+    credentials: true,
+  })
+);
+app.options("*", cors());
+
+
+const PORT = process.env.PORT || 3000;
 
 // Connect to MongoDB
-connectDB()
-
-
+connectDB();
 
 //Admin
 app.use("/api/admin/users", adminRoutes);
@@ -32,7 +40,7 @@ app.use("/api/admin/products", productAdminRoutes);
 app.use("/api/admin/orders", adminOrderRoutes);
 
 //API Routes
-app.use("/api/users", userRoutes)
+app.use("/api/users", userRoutes);
 app.use("/api/products", productRouter);
 app.use("/api/cart", cartRouter);
 app.use("/api/checkout", checkoutRouter);
@@ -40,8 +48,6 @@ app.use("/api/orders", orderRouter);
 app.use("/api/upload", uploadRouter);
 app.use("/api", subscriberRouter);
 
-
-app.listen(PORT , ()=>{
-  console.log(`Server is running on http://localhost:${PORT} `)
-})
-
+app.listen(PORT, () => {
+  console.log(`Server  running on post: ${PORT} `);
+});
